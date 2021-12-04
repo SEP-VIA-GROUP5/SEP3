3d3b533e33cc944e431d90aa35ff876d48debdf8
rycka13 <71268611+rycka13@users.noreply.github.com>
2021-12-04 12:36:33 +0100

    fixed minor details

--- C#/WebSiteSEP3/Client/Data/IGameService.cs
+++ C#/WebSiteSEP3/Client/Data/IGameService.cs
@@ -10,7 +10,7 @@ namespace Client.Data
         Task<string> getReceiptAsync(User user, Game game);
         Task<GameKey> getProductKeyAsync(int gameId);
         Task<Game> addGameAsync(Game game);
-        Task<string> addGameKeyAsync(int gameId, string productKey);
+        Task<GameKey> addGameKeyAsync(int gameId, string productKey);
         Task<GameCluster> getSearchAsync(string search);
         Task addGameToShoppingCartAsync(User user, int gameId);
         Task<GameCluster> removeGameFromShoppingCartAsync(User user, int gameId);
